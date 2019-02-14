2d91c9289be1df14fb68694cff2171b92efe3e2a
jimmywit3 <jimmywit3@gmail.com>
2019-02-14 12:48:30 +0300

    Updates

---
+++ precache-manifest.ce8b02ff0eeaba50536815d3c567a4da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e5169869dddccd340c1",
+    "url": "/CardFlip/static/css/main.e49935b2.chunk.css"
+  },
+  {
+    "revision": "3e5169869dddccd340c1",
+    "url": "/CardFlip/static/js/main.3e516986.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/CardFlip/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "8c208c1dbf347c766c10",
+    "url": "/CardFlip/static/js/runtime~main.8c208c1d.js"
+  },
+  {
+    "revision": "495d96bd67e33e2fa2bff41f5c965a32",
+    "url": "/CardFlip/index.html"
+  }
+];
